18fbd85a5b4f95fe40a47b3fb143c64c9dcc8238
Petros Tsimvris <tsimvris@gmail.com>
2022-08-02 14:08:53 +0200

    created Veggie and Dessert pages

---
+++ pages/Dessert.js
@@ -0,0 +1,14 @@
+export default function Dessert() {
+  return (
+    <div>
+      <h2>Unsere Desserts</h2>
+      <hr />
+      <p>
+        <ul>
+          <li>Schwarzwälder Torte</li>
+          <li>Vegane Donuts</li>
+        </ul>
+      </p>
+    </div>
+  );
+}
---
+++ pages/Veggie.js
@@ -0,0 +1,14 @@
+export default function Veggie() {
+  return (
+    <div>
+      <h2>Unsere Veggie Produkte</h2>
+      <hr />
+      <p>
+        <ul>
+          <li>Veggie Patties</li>
+          <li>Veggie Wurst aus echte Schwein</li>
+        </ul>
+      </p>
+    </div>
+  );
+}
