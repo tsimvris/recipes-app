9ab54239f0390b0fd20b33c2bd758ecc3937fe4b
Petros Tsimvris <tsimvris@gmail.com>
2022-08-02 15:26:42 +0200

    fixed links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,21 @@
 import StyledHeader from "../StyledComponents/StyledHeader";
 import StyleNav from "../StyledComponents/StyledNav";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <StyledHeader>
       <StyleNav>
-        <a href="./">Home</a>
-        <a href="Veggie">Veggie</a>
-        <a href="Dessert">Desserts</a>
+        <Link href="./">
+          <a>Home</a>
+        </Link>
+        <Link href="Veggie">
+          <a>Veggie</a>
+        </Link>
+
+        <Link href="Dessert">
+          <a>Desserts</a>
+        </Link>
       </StyleNav>
     </StyledHeader>
   );
